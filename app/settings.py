e71e9715f515f635fa1dcf19e44e17508b1c1261
Daniel <danielbattisti@Outlook.com.br>
2023-09-14 16:11:18 -0300

    first-commit

--- app/settings.py
+++ app/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'app.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'DofusShop',
+        'NAME': 'dofusshop',
         'USER': 'postgres',
         'PASSWORD': '070304aBc',
         'HOST': 'localhost',
